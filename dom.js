1b5b45bd3f713ddeb28e4daea00ea90636ed1990
Siyanda <siyalandz@gmail.com>
2019-07-15 13:05:07 +0200

    my edit

--- dom.js
+++ dom.js
@@ -5,15 +5,15 @@ var setNameElem = document.querySelector('setName')
 var getNameElem = document.querySelector('getName')
 var languageElem = document.querySelector('language')
 
-var msg = document.getElementById('msg'); // displaying message
+var rd1 = document.getElementById('msg'); // displaying message
 
 
 function textBtnClicked() {
     var userName = document.querySelector(".userName"); //get user input text value
-    var l = document.querySelector("input[name='username']:checked");
+     var elem  = document.querySelector("input[name='greetBtn']:checked");
 
-    msg.innerHTML = "Hallo, " + userName.value + "!"
-    msg.innerHTML = "Hello, " + userName.value + "!" // displaye greeting message
+     msg.innerHTML = "Hallo, " + userName.value + "!"
+     msg.innerHTML = "Hello, " + userName.value + "!" // displaye greeting message
     msg.innerHTML = "molo, " + userName.value + "!" 
    
     // if (lang.value === "Hello, userName.value") {
